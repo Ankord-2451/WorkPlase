8ee1900ded4e7db3dc2055cd76a46e1635ff470c
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-07-15 18:32:38 +0300

    Added Task and Project Controller

---
+++ WorkPlase1/Controllers/ProjectController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WorkPlase1.Data;
+using WorkPlase1.Models;
+
+namespace WorkPlase1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private ApplicationDbContext dbContext;
+
+        public ProjectController(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProjectModel> Get([FromBody] int IdOfEmployer)
+        {
+            return dbContext.Projects.Where(x => x.IDofEmployer == IdOfEmployer).ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ProjectModel project)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Projects.Add(project);
+                dbContext.SaveChanges();
+                return Ok(JsonSerializer.Serialize<string>("Was created new project"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("project isn't valid"));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] ProjectModel project)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dbContext.Projects.Remove(dbContext.Projects.First(x => x.Id == project.Id));
+                }
+                catch
+                {
+                    return Ok(JsonSerializer.Serialize<string>("project wasn't found"));
+                }
+                dbContext.Projects.Add(project);
+                dbContext.SaveChanges();
+                return Ok(JsonSerializer.Serialize<string>("project was update"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("project isn't valid"));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] int Id)
+        {
+            try
+            {
+                dbContext.Projects.Remove(dbContext.Projects.First(x => x.Id == Id));
+            }
+            catch
+            {
+                return Ok(JsonSerializer.Serialize<string>("project wasn't found"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("project was delete"));
+        }
+    }
+}
---
+++ WorkPlase1/Controllers/TaskController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WorkPlase1.Data;
+using WorkPlase1.Models;
+
+namespace WorkPlase1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private ApplicationDbContext dbContext;
+        public TaskController (ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<TaskModel> Get([FromBody]int IdOfPoject)
+        {
+            return dbContext.Tasks.Where(x=>x.IDofProject== IdOfPoject).ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Tasks.Add(task);
+                dbContext.SaveChanges();
+                return Ok(JsonSerializer.Serialize<string>("Was created new Task"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("Task isn't valid"));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                try { 
+                dbContext.Tasks.Remove(dbContext.Tasks.First(x=>x.Id==task.Id));
+                }
+                catch {
+                    return Ok(JsonSerializer.Serialize<string>("Task wasn't found"));
+                }
+                dbContext.Tasks.Add(task);
+                dbContext.SaveChanges();
+                return Ok(JsonSerializer.Serialize<string>("Task was update"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("Task isn't valid"));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody]int Id)
+        {
+            try
+            {
+                dbContext.Tasks.Remove(dbContext.Tasks.First(x => x.Id == Id));
+                dbContext.SaveChanges();
+            }
+            catch
+            {
+                return Ok(JsonSerializer.Serialize<string>("Task wasn't found"));
+            }
+            return Ok(JsonSerializer.Serialize<string>("Task was delete"));
+        }
+    }
+}
